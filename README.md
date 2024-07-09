b84df64da609dc06cf3353d15f548dc5d984055d
juangh27 <101841901+juangh27@users.noreply.github.com>
2024-07-09 16:47:21 -0600

    dashboard from MUI base and first edit'

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react_cms
 CMS front made with react
+
+
+npm run dev
---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Dashboard from './Dashboard/Dashboard'
+
+function App() {
+
+  return (
+    <>
+<Dashboard />
+    </>
+  )
+}
+
+export default App
